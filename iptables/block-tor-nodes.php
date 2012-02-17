e9ac59afee10c8e26391ba46bd2a8be2978dc841
Ronald Ning <the@loude.st>
2012-02-17 13:38:03 -0800

    Sample shell script to block Tor nodes

---
+++ iptables/block-tor-nodes.php
@@ -0,0 +1,18 @@
+<?php	
+	$url = 'http://torstatus.blutmagie.de/ip_list_all.php/Tor_ip_list_ALL.csv';
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL, $url); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101209 Firefox/3.6.13'); 
+	$data = curl_exec($ch); 
+	curl_close($ch); 
+	$ips = explode("\n", $data);
+	echo "sudo iptables -F\n";
+	echo "sudo iptables -t nat -F\n";
+	echo "sudo iptables -X\n";
+	foreach($ips as $ip) {
+		if(trim($ip) != "" and trim($ip) != "67.136.217.102") {
+			echo "sudo iptables -A INPUT -s ".$ip." -j DROP\n";
+		}
+	}
+?>
